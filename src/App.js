3e219e260e1a0a42840c9ecb0843b7aeaf303ff2
dbaigalmaa <dbaigalmaa@gmail.com>
2023-02-14 08:41:25 -0800

    Add Github repo on footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This app is coded by Bella and{" "}
+        <a
+          href="https://github.com/dbaigalmaa/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
