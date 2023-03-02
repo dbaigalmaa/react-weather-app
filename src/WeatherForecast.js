1b9c923fd8547210e8708bdf6da9ede83dadd43e
dbaigalmaa <dbaigalmaa@gmail.com>
2023-03-02 10:17:58 -0800

    fixed error

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -22,13 +22,13 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            while (index < 5) {
+            if (index > 5) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
-            }
+            } else return null;
           })}
         </div>
       </div>
